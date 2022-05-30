7deb8e7c94f3428a6cf6402bb43b8babd268ff14
Wilfred <wilfred05777@gmail.com>
2022-05-30 15:22:56 +0800

    268. Collection Product Component - bug on keys

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import React, { Component, Fragment } from "react";
 import Categories from "../components/home/Categories";
+import Collection from "../components/home/Collection";
 import FeaturedProducts from "../components/home/FeaturedProducts";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        {/* <h1>Home Page </h1> */}
         <FeaturedProducts />
+        <Collection />
         <Categories />
       </Fragment>
     );
