7f728ad57e508109b6cd50815bee22f848c4d80a
Wilfred <wilfred05777@gmail.com>
2022-06-02 21:46:50 +0800

    274. Nav Menu Desktop Part 1

--- user/ecom/src/components/common/NavMenuDesktop.jsx
+++ user/ecom/src/components/common/NavMenuDesktop.jsx
@@ -1,12 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Col, Container, Navbar, Row } from "react-bootstrap";
+import Logo from "../../assets/images/easyshop.png";
+import { Link } from "react-router-dom";
 
 export class NavMenuDesktop extends Component {
   render() {
     return (
-      <div>
-        NavMenuDesktop
-        <div></div>
-      </div>
+      <Fragment>
+        <div className="TopSectionDown">
+          <Navbar className="navbar" fixed={"top"} bg="light">
+            <Container fluid={"true"}>
+              <Row>
+                <Col>
+                  <Link to="/">
+                    <img className="nav-logo" src={Logo} />
+                  </Link>
+                </Col>
+                <Col lg={4} md={4} sm={12} sx={12}></Col>
+                <Col lg={4} md={4} sm={12} sx={12}></Col>
+              </Row>
+              {/* <Navbar.Brand href="#home">Brand link</Navbar.Brand> */}
+            </Container>
+          </Navbar>
+        </div>
+      </Fragment>
     );
   }
 }
