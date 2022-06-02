cceebedd8a30eea8fe6891b1b0ef73ebbf03e594
Wilfred <wilfred05777@gmail.com>
2022-06-02 22:09:54 +0800

    275. Nav Menu Desktop Part 2

--- user/ecom/src/components/common/NavMenuDesktop.jsx
+++ user/ecom/src/components/common/NavMenuDesktop.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Col, Container, Navbar, Row } from "react-bootstrap";
+import { Button, Col, Container, Navbar, Row } from "react-bootstrap";
 import Logo from "../../assets/images/easyshop.png";
 import { Link } from "react-router-dom";
 
@@ -9,17 +9,62 @@ export class NavMenuDesktop extends Component {
       <Fragment>
         <div className="TopSectionDown">
           <Navbar className="navbar" fixed={"top"} bg="light">
-            <Container fluid={"true"}>
+            <Container
+              fluid={"true"}
+              className="fixed-top shadow-sm p-2 mb-0 bg-white"
+            >
               <Row>
                 <Col>
                   <Link to="/">
                     <img className="nav-logo" src={Logo} />
                   </Link>
                 </Col>
-                <Col lg={4} md={4} sm={12} sx={12}></Col>
-                <Col lg={4} md={4} sm={12} sx={12}></Col>
+                <Col className="p-1 mt-1" lg={4} md={4} sm={12} sx={12}>
+                  <div className="input-group w-100">
+                    <input type="text" className="form-control" />
+
+                    <Button>
+                      <i className="fa fa-search"></i>
+                    </Button>
+                  </div>
+                </Col>
+                <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                  <Link to="/" className="btn">
+                    <i className="fa h4 fa-bell"></i>
+                    <sup>
+                      <span className="badge text-white bg-danger">5</span>
+                    </sup>
+                  </Link>
+                  <a className="btn">
+                    <i className="fa h4 fa-mobile-alt"></i>
+                  </a>
+                  <Link to="/" className="h4 btn">
+                    LOGIN
+                  </Link>
+
+                  <Button className="cart-btn">
+                    <i className="fa fa-shopping-cart"></i> 3 Items
+                  </Button>
+                </Col>
+                {/* <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                  <Link to="/" className="btn">
+                    <i className="fa h4 fa-bell"></i>
+                    <sup>
+                      <span className="badge text-white bg-danger">5</span>
+                    </sup>
+                  </Link>
+                  <a className="btn">
+                    <i className="fa h4 fa-mobile-alt"></i>
+                  </a>
+                  <Link to="/" className="h4 btn">
+                    LOGIN
+                  </Link>
+
+                  <Button className="cart-btn">
+                    <i className="fa fa-shopping-cart"></i> 3 Items{" "}
+                  </Button>
+                </Col> */}
               </Row>
-              {/* <Navbar.Brand href="#home">Brand link</Navbar.Brand> */}
             </Container>
           </Navbar>
         </div>
