d7694aa657d7eac691d4ac524199c423ef5312cf
Wilfred <wilfred05777@gmail.com>
2022-06-16 05:10:07 +0800

    277. Mobile View Menu Part 2

--- user/ecom/src/components/common/NavMenuDesktop.jsx
+++ user/ecom/src/components/common/NavMenuDesktop.jsx
@@ -2,8 +2,40 @@ import React, { Component, Fragment } from "react";
 import { Button, Col, Container, Navbar, Row } from "react-bootstrap";
 import Logo from "../../assets/images/easyshop.png";
 import { Link } from "react-router-dom";
+import MegaMenuAll from "../home/MegaMenuAll";
 
 export class NavMenuDesktop extends Component {
+  constructor() {
+    super();
+    this.state = {
+      SideNavState: "sideNavClose",
+      ContentOverState: "ContentOverlayClose",
+    };
+  }
+
+  MenuBarClickHandler = () => {
+    this.SideNavOpenClose();
+  };
+
+  ContentOverlayClickHandler = () => {
+    this.SideNavOpenClose();
+  };
+
+  SideNavOpenClose = () => {
+    let SideNavState = this.state.SideNavState;
+    let ContentOverState = this.state.ContentOverState;
+    if (SideNavState === "sideNavOpen") {
+      this.setState({
+        SideNavState: "sideNavClose",
+        ContentOverState: "ContentOverlayClose",
+      });
+    } else {
+      this.setState({
+        SideNavState: "sideNavOpen",
+        ContentOverState: "ContentOverlayOpen",
+      });
+    }
+  };
   render() {
     return (
       <Fragment>
@@ -14,7 +46,10 @@ export class NavMenuDesktop extends Component {
               className="fixed-top shadow-sm p-2 mb-0 bg-white"
             >
               <Row>
-                <Col>
+                <Col lg={4} md={4} sm={12} xs={12}>
+                  <Button onClick={this.MenuBarClickHandler} className="btn">
+                    <i className="fa fa-bars"></i>
+                  </Button>
                   <Link to="/">
                     <img className="nav-logo" src={Logo} />
                   </Link>
@@ -46,24 +81,6 @@ export class NavMenuDesktop extends Component {
                     <i className="fa fa-shopping-cart"></i> 3 Items
                   </Button>
                 </Col>
-                {/* <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
-                  <Link to="/" className="btn">
-                    <i className="fa h4 fa-bell"></i>
-                    <sup>
-                      <span className="badge text-white bg-danger">5</span>
-                    </sup>
-                  </Link>
-                  <a className="btn">
-                    <i className="fa h4 fa-mobile-alt"></i>
-                  </a>
-                  <Link to="/" className="h4 btn">
-                    LOGIN
-                  </Link>
-
-                  <Button className="cart-btn">
-                    <i className="fa fa-shopping-cart"></i> 3 Items{" "}
-                  </Button>
-                </Col> */}
               </Row>
             </Container>
           </Navbar>
---
+++ user/ecom/src/components/home/MegaMenuAll.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class MegaMenuAll extends Component {
+  render() {
+    return <div>MegaMenuAll</div>;
+  }
+}
+
+export default MegaMenuAll;
